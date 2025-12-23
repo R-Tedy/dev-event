d3fd54a9b4da42829c1b7b7e4a42061790f3c80d
R-Tedy <stedyroland2@gmail.com>
2025-12-24 02:19:47 +0300

    impliment the explore btn

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+'use client'
+
+import Image from "next/image"
 
 const ExploreBtn = () => {
   return (
-    <div>ExploreBtn</div>
+    <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log('Click')}>
+      <a href="#events">
+        Explore Events
+        <Image src={'/icons/arrow-down.svg'} alt="arrow-down" width={24} height={24}/>
+      </a>
+    </button>
   )
 }
 
