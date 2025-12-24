03d949ccb5e26ca545da8ec9bcbc03fec944b0af
R-Tedy <stedyroland2@gmail.com>
2025-12-25 02:43:04 +0300

    create event routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
-const Page = () => {
+import { cacheLife } from "next/cache";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Page = async () => {
+  // 'use cache'
+  // cacheLife('hours');
+  // const response = await fetch(``)
   return (
     <section>
       <h1 className='text-center'>The Hub for Every Developer <br /> Event You Can&apos;t Miss</h1>
